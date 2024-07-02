93926dcada46d54c10028b9e45633e5d68f7593f
sejongPark <lifedesigner88@gmail.com>
2024-07-02 11:22:32 +0900

    pug iteration Array []

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,14 @@
 const fakeUser = {
     username: "Sejong Park",
-    loggedIn: false,
+    loggedIn: true,
 }
 
 export const trending = (req, res) => {
+    const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     return res.render("home", {
         pageTitle: "Home",
-        fakeUser
+        fakeUser,
+        videos
     });
 }
 export const see = (req, res) => {
