2d229389e27ec48cb1b25da4d92d4c5a837b3c5c
sejongPark <lifedesigner88@gmail.com>
2024-07-03 16:39:20 +0900

    Edit Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,35 +3,37 @@ const fakeUser = {
     loggedIn: true,
 }
 
+const videosObject = [
+
+    {
+        title: "Video 1",
+        rating: 5,
+        comments: 2,
+        createdAt: "3 minutes ago",
+        views: 59,
+        id: 1
+    },
+    {
+        title: "Video 2",
+        rating: 4,
+        comments: 7,
+        createdAt: "2 minutes ago",
+        views: 1,
+        id: 2
+    },
+    {
+        title: "Video 3",
+        rating: 2,
+        comments: 5,
+        createdAt: "1 minutes ago",
+        views: 53,
+        id: 3
+    }
+];
+
 export const trending = (req, res) => {
     const videosNumList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    const videosObject = [
 
-        {
-            title: "Video 1",
-            rating: 5,
-            comments: 2,
-            createdAt: "3 minutes ago",
-            views: 59,
-            id: 1
-        },
-        {
-            title: "Video 2",
-            rating: 4,
-            comments: 7,
-            createdAt: "2 minutes ago",
-            views: 69,
-            id: 3
-        },
-        {
-            title: "Video 3",
-            rating: 2,
-            comments: 5,
-            createdAt: "1 minutes ago",
-            views: 53,
-            id: 2
-        }
-    ];
     return res.render("home", {
         pageTitle: "Home",
         fakeUser,
@@ -39,11 +41,20 @@ export const trending = (req, res) => {
         videosObject
     });
 }
-export const see = (req, res) => {
-    return res.render("watch");
+export const watch = (req, res) => {
+    const {videoId} = req.params;
+    const video = videosObject[videoId - 1];
+    return res.render("watch", {
+        pageTitle: `Watching ${video.title}`,
+        fakeUser,
+        video
+    });
 }
 export const edit = (req, res) => {
-    return res.render("edit");
+    return res.render("edit", {
+        pageTitle: `Edit User `,
+        fakeUser,
+    });
 }
 
 export const search = (req, res) => {
@@ -59,3 +70,4 @@ export const deleteVideo = (req, res) => {
     return res.send("<h1> delete Videos </h1>");
 }
 
+// https://nomadcoders.co/wetube/lectures/2692
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
-import {deleteVideo, edit, see, upload} from "../controllers/videoController"
+import {deleteVideo, edit, watch, upload} from "../controllers/videoController"
 
 
 // videos
 const videoRouter = express.Router();
 videoRouter.get('/upload', upload);
-videoRouter.get('/:videoId(\\d+)', see);
+videoRouter.get('/:videoId(\\d+)', watch);
 videoRouter.get('/:videoId(\\d+)/edit', edit);
 videoRouter.get('/:videoId(\\d+)/delete', deleteVideo);
 export default videoRouter;
