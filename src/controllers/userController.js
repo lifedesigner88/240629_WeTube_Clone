d2056659f189a3633c1264c7e8c8cebdf8f8a447
sejongPark <lifedesigner88@gmail.com>
2024-07-11 17:59:23 +0900

    AOuth Setting

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -54,7 +54,7 @@ export const getLogin = (req, res) => {
 export const postLogin = async (req, res) => {
     const {username, password} = req.body;
     const user = await User.findOne({username})
-    if(!user)
+    if (!user)
         return res.status(400).render("login", {
             pageTitle: "Login", errorMessage: "username Does not exists"
         });
@@ -90,5 +90,59 @@ export const remove = (req, res) => {
     return res.send("<h1> Remove User </h1>");
 };
 
+require('dotenv').config()
 
-// https://nomadcoders.co/wetube/lectures/2711
+// AOuth
+export const startGithubLogin = (req, res) => {
+    const baseUrl = "https://github.com/login/oauth/authorize";
+    const config = {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        allow_signup: true,
+        scope: "read:user user:email",
+    };
+    const params = new URLSearchParams(config).toString();
+    const githubLoginUrl = `${baseUrl}?${params}`;
+    return res.redirect(githubLoginUrl);
+}
+
+export const finishGithubLogin = async (req, res) => {
+
+    const {code} = req.query;
+
+    const baseUrl = "https://github.com/login/oauth/access_token";
+    const config = {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code,
+    };
+
+    const params = new URLSearchParams(config).toString();
+
+    try {
+
+        // Access Token 요청
+        const accessTokenUrl = `${baseUrl}?${params}`;
+        const response = await fetch(accessTokenUrl, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json'
+            }
+        });
+        const data = await response.json();
+        const access_token = data.access_token;
+
+        // Access Token 으로 유저 정보 요청
+        const apiUrl = "https://api.github.com";
+        const userData = await fetch(`${apiUrl}/user`, {
+            headers: {
+                Authorization: `token ${access_token}`
+            }
+        });
+        const userJson = await userData.json();
+        console.log("😊", userJson);
+
+    } catch (error) {
+        console.log("Github OAuth Error", error);
+        return res.redirect("/login")
+    }
+}
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {remove, edit, logout, see} from "../controllers/UserController";
+import {remove, edit, logout, see, startGithubLogin ,finishGithubLogin} from "../controllers/UserController";
 
 
 // users
@@ -7,6 +7,8 @@ const userRouter = express.Router();
 userRouter.get("/logout", logout);
 userRouter.get('/edit', edit);
 userRouter.get("/remove", remove);
+userRouter.get("/github/start", startGithubLogin)
+userRouter.get("/github/finish", finishGithubLogin)
 userRouter.get("/:id", see);
 export default userRouter;
 
