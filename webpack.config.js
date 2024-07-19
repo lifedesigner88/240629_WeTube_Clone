7e4b5afc8e79d25fd6745f92db982d31f5711234
sejongPark <lifedesigner88@gmail.com>
2024-07-19 13:26:23 +0900

    Extrac CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
     entry: "./src/client/js/main.js",
+    plugins: [new MiniCssExtractPlugin({
+        filename: "css/style.css",
+    })],
     mode: "development",
     output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "assets", "js"),
+        filename: "js/main.js",
+        path: path.resolve(__dirname, "assets"),
     },
     module: {
         rules: [
@@ -19,7 +24,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             },
         ],
     },
